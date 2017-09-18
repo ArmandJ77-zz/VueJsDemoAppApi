1bdf41cc1dd60ad16caed9949e577df6c1990d73
Armand Jordaan <armandjordaan@live.co.za>
2017-09-18 13:24:57 +0200

    Added new crud enpoints

--- DemoApiProject/Controller/ClientController.cs
+++ DemoApiProject/Controller/ClientController.cs
@@ -1,4 +1,6 @@
-﻿using DemoApiProject.Data;
+﻿using System;
+using System.Net;
+using DemoApiProject.Data;
 using DemoApiProject.Infrastructure;
 using DemoApiProject.Model;
 using Microsoft.AspNetCore.Mvc;
@@ -7,17 +9,62 @@ using System.Threading.Tasks;
 
 namespace DemoApiProject.Controller
 {
-  public class ClientController : BaseController
-  {
-    public ClientController(ApplicationDbContext context) : base(context)
+    public class ClientController : BaseController
     {
+        public ClientController(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> Get() => new JsonResult(await Context.Clients.ToListAsync());
+
+        [HttpGet]
+        [Route("/api/[controller]/GetPaginated")]
+        public async Task<JsonResult> GetPaginated(int? page, int pageSize) => new JsonResult(await PaginatedList<Client>.CreateAsync(Context.Clients.AsNoTracking(), page ?? 1, pageSize));
+
+        [HttpPost]
+        public async Task<JsonResult> Create(Client clientDto)
+        {
+            try
+            {
+                await Context.Clients.AddAsync(clientDto);
+                await Context.SaveChangesAsync();
+                return new JsonResult(new { message = "Saved" });
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new JsonResult(new { message = "Failed" });
+            }
+        }
+
+        [HttpDelete]
+        public async Task<JsonResult> Delete(int? Id)
+        {
+            if (Id == null)
+                return new JsonResult(NotFound());
+
+            var client = await Context.Clients
+                .AsNoTracking()
+                .SingleOrDefaultAsync(x => x.Id == Id);
+
+            if (client == null)
+                return new JsonResult(NotFound());
+
+            try
+            {
+                Context.Clients.Remove(client);
+                await Context.SaveChangesAsync();
+                return new JsonResult(Ok());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new JsonResult(HttpStatusCode.InternalServerError);
+            }
+        }
+
+
     }
-
-    [HttpGet]
-    public async Task<JsonResult> Get() => new JsonResult(await Context.Clients.ToListAsync());
-
-    [HttpGet]
-    [Route("/api/[controller]/GetPaginated")]
-    public async Task<JsonResult> GetPaginated(int? page, int pageSize) => new JsonResult(await PaginatedList<Client>.CreateAsync(Context.Clients.AsNoTracking(), page ?? 1, pageSize));
-  }
 }
